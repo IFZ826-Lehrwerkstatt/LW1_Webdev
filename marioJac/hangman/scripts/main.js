2ea5af9b48b25c6a5c685f7924bbbf604b9dc604
Motzer Jacqueline <jacqueline.motzer@wiss-online.ch>
2019-09-18 15:28:52 +0200

    dictionary in main.js

--- marioJac/hangman/scripts/main.js
+++ marioJac/hangman/scripts/main.js
@@ -9,15 +9,50 @@ var counter = 0;
 var elem;
 
 
+var hangman_dictionary = {
+
+    "M101": { "Datentyp": "eine Komponente der Variablendeklaration",
+      "Rückgabetyp": "Bestandteil Funktionskopf"},
+    "M104": { "Datentyp": "eine Komponente der Variablendeklaration",
+      "Rückgabetyp": "Bestandteil Funktionskopf"},
+    "M105": { "Grafik": "eine visuelle Darstellung im Web",
+      "Rückgabetyp": "Bestandteil Funktionskopf"},
+    "M133": { "Datentyp": "eine Komponente der Variablendeklaration",
+      "Rückgabetyp": "Bestandteil Funktionskopf"},
+    "M152": { "Grafik": "eine visuelle Darstellung im Web",
+      "Rückgabetyp": "Bestandteil Funktionskopf"},
+    "M226A": { "Klasse": "Bauplan für gleichartige Objekte",
+      "Objekt": "Ausprägung bzw. Instanz einer Klasse",
+      "Konstruktor": "Klassenfunktion zum Erstellen eines Objekts"},
+    "M307": { "Grafik": "eine visuelle Darstellung im Web",
+      "Rückgabetyp": "Bestandteil Funktionskopf"},
+    "M403": { "Datentyp": "eine Komponente der Variablendeklaration",
+      "Variablenname": "eine Komponente Variablendeklaration",
+      "Parameterliste": "Bestandteil vom Funktionskopf",
+      "Rückgabetyp": "Bestandteil Funktionskopf"
+  }
+};
+
+$('.btnModul').on('click', function(){
+  /*HIER DAS DICTIONARY LADEN*/
+  var modul = $(this).val();
+  var keys = Object.keys(hangman_dictionary[modul]);
+  word = keys[Math.floor(Math.random() * keys.length)];
+  var description = hangman_dictionary[modul][word];
+  $('#hint').text(description);
+  console.log("Word: " + word);
+});
+
+
 
 
 //Funktion - Gibt das Wort mit Underlines aus
+
+
 window.addEventListener('load', function() {
 
   //Wort wird aus Array geholt
-  var words = ["DATEN"];
-  var rand = Math.floor(Math.random()*words.length);
-  word = words[rand];
+
 
   //Die einzelnen Buchstabenm sowie die nötige Anzahl Underlines werden in je ein Array gepusht
   for(var x = 0; x < word.length; x++) {
@@ -47,33 +82,15 @@ window.onload = function(e) {
 
 var checkChar = function() {
   check = true;
-  var usedLetters = document.getElementById('usedLetters').innerHTML;;
+
 
   if($(elem).hasClass('disable')) {
   }
 
   else {
 
-    if(usedLetters.includes(char)) {
-      console.log("contains");
-
-    }
-    else {
-      console.log("arrUsedLetters.length: " + arrUsedLetters.length);
-      for(var x = 0; x <= arrUsedLetters.length; x++) {
-        if(arrUsedLetters[x] == char) {
-          check = false;
-          console.log(check);
-        }
-
-        else {
-          arrUsedLetters.push(char);
-        }
-
         if(check) {
 
-
-          document.getElementById('usedLetters').innerHTML = arrUsedLetters.join(' ');
           console.log("arrChar.length: " + arrChar.length);
           for(var x = 0; x < arrChar.length; x++) {
             if(arrChar[x] == char) {
@@ -83,11 +100,6 @@ var checkChar = function() {
           document.getElementById('getWord').innerHTML = arrUnderL.join(' ');
         }
 
-
-      }
-      usedLetters = document.getElementById('usedLetters').innerHTML;
-      console.log(usedLetters);
-    }
   }
   elem.classList.add('disable');
 }
